cf29a870777a8e50a3b4c8c91655769f1910ef88
Daniel81W <it@dan-web.de>
2024-03-12 19:49:10 +0100

    Upd

---
+++ DWIPSModBusSlave/module.php
@@ -0,0 +1,57 @@
+<?php
+    /** @noinspection PhpExpressionResultUnusedInspection */
+    /** @noinspection PhpUnused */
+    /** @noinspection PhpRedundantClosingTagInspection */
+
+    class DWIPSModBusSlave extends IPSModule {
+
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+
+
+
+		}
+
+		/**
+        * @return void
+        */
+        public function Destroy()
+		{
+			//Never delete this line!
+			parent::Destroy();
+
+		}
+
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+
+		}
+
+
+        public function GetConfigurationForm():string{
+            return "";
+        }
+
+		/**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wie folgt zur Verfügung gestellt:
+        *
+        * DWIPSShutter_UpdateSunrise($id);
+        *
+        */
+
+		public function ReceiveData($JSONString) {
+			
+		}
+
+		public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
+
+
+		}
+		
+    }
+?>
