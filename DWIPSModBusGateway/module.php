73c2ec22cc9a394cb7d835841de3c2c95824a561
Daniel81W <it@dan-web.de>
2024-06-06 14:15:19 -0500

    Upd

--- DWIPSModBusGateway/module.php
+++ DWIPSModBusGateway/module.php
@@ -10,7 +10,7 @@
 			//Never delete this line!
 			parent::Create();
 
-            $this->RegisterPropertyInteger("ModbusType", 0);
+            $this->RegisterPropertyInteger("ModbusType", ModBusType::ModBus_TCP);
             $this->RegisterPropertyInteger("DeviceID", 1);
 		}
 
@@ -29,6 +29,24 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 
+            switch ($this->ReadPropertyInteger("ModbusType")) {
+                case ModBusType::ModBus_TCP:
+                case ModBusType::ModBus_RTU_TCP:
+                case ModBusType::ModBus_ASCII_TCP:
+                    $this->ForceParent("{8062CF2B-600E-41D6-AD4B-1BA66C32D6ED}");
+                    break;
+                case ModBusType::ModBus_UDP:
+                case ModBusType::ModBus_RTU_UDP:
+                case ModBusType::ModBus_ASCII_UDP:
+                    $this->ForceParent("{82347F20-F541-41E1-AC5B-A636FD3AE2D8}");
+                    break;
+                case ModBusType::ModBus_RTU:
+                case ModBusType::ModBus_ASCII:
+                    $this->ForceParent("{6DC3D946-0D31-450F-A8C6-C42DB8D7D4F1}");
+                    break;
+                default:
+                    break;
+            }
 		}
 
 
@@ -54,4 +72,17 @@
 		}
 		
     }
+
+class ModBusType
+{
+    const ModBus_TCP = 0;
+    const ModBus_UDP = 1;
+    const ModBus_RTU = 2;
+    const ModBus_RTU_TCP = 3;
+    const ModBus_RTU_UDP = 4;
+    const ModBus_ASCII = 5;
+    const ModBus_ASCII_TCP = 6;
+    const ModBus_ASCII_UDP = 7;
+
+}
 ?>
