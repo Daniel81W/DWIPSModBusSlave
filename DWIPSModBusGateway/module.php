33d200883216f082fc94cd3c8ad311fa9fad93c7
Daniel81W <it@dan-web.de>
2024-06-06 14:54:07 -0500

    Upd

--- DWIPSModBusGateway/module.php
+++ DWIPSModBusGateway/module.php
@@ -85,6 +85,8 @@
             $this->SendDebug("Data", $clientPort, 0);
             $broadcast = boolval($udpdata['Broadcast']);
             $this->SendDebug("Data", $broadcast, 0);
+            $buffer = $udpdata['Buffer'];
+            $this->SendDebug("Data", $buffer, 0);
             //$this->SendDebug("Data", print_r($data, true), 0);
             //$this->SendDebug("Data", $data['Buffer'], 0);
         }
